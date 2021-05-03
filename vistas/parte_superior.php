52d6bc1e3518308a893d6c5f60c86a880990836c
ROCK3D3L <62083972+ROCK3D3L@users.noreply.github.com>
2021-05-03 14:21:40 -0400

    Update parte_superior.php

--- vistas/parte_superior.php
+++ vistas/parte_superior.php
@@ -36,38 +36,25 @@
     <!-- Sidebar -->
     <ul class="navbar-nav roque  sidebar  accordion" id="accordionSidebar">
 
-      <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon ">
 
           <img class="img-fluid"  src="img/LogoFinal.png" alt="logo">
-          <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
         <div class="sidebar-brand-text text-black mx-3"> CONÓCELOS </div>
       </a>
 
-      <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
         <a class="nav-link " href="index.php">
           <i  class="fas fa-home" style="color: black;"></i>
           <span class="text-black">Home</span></a>
       </li>
 
-      <!-- Divider -->
       <hr class="sidebar-divider">
 
-      <!-- Heading -->
-      <!-- <div class="sidebar-heading">
-        Interface
-      </div> -->
-
-      <!-- Nav Item - Pages Collapse Menu -->
-     
-
-      <!-- Nav Item - Utilities Collapse Menu -->
+    
       <li class="nav-item">
         <a class="nav-link " href="contenido/movimientos.php" >
         <i class="fas fa-users" style="color: black;"></i>
@@ -75,7 +62,6 @@
         </a>
         
       </li>
-      <!-- nav Item Informes -->
       <li class="nav-item">
         <a class="nav-link "  >
           <i class="fas fa-list fa-sm" style="color: black;"></i>
@@ -103,7 +89,6 @@
         </a>
        
       </li>
-      <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
 
       <!-- Sidebar Toggler (Sidebar) -->
@@ -131,12 +116,7 @@
            <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
 
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            
-            <!-- Nav Item - Alerts -->
-            
 
-            <!-- Nav Item - User Information -->
   
           </ul>
 
