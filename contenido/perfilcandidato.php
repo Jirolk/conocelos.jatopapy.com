3e8f95f48c1d4f4dc0ff29b338b4c5ea01d0bc23
ROCK3D3L <62083972+ROCK3D3L@users.noreply.github.com>
2021-05-09 21:24:45 -0400

    Update perfilcandidato.php

--- contenido/perfilcandidato.php
+++ contenido/perfilcandidato.php
@@ -52,7 +52,7 @@
             <hr class="divider">';
 
         echo '
-        <div class="row">
+        <div id="datPers" class="row">
           <div class="col">
                     <div class="card shadow mb-2  ">
                       <div class="card py-3 r3 align-items-center">
@@ -64,7 +64,7 @@
             </div>
         </div>';
          echo '
-         <div class="row">
+         <div id="datPersDet" class="row">
             <div class="col">
                 <div class="card shadow mb-2  ">
                     <div class="card py-2 r2 align-items-center">
@@ -269,6 +269,40 @@
 <?php require_once "vistas/parte_inferior.php"; ?>
 <script src="../js/demo/chart.min.js"></script>
 <!-- </body> -->
+<script>
+  function habilitar(value) {
+      var sele = document.getElementById("filtro");
+      if (sele.options[sele.selectedIndex].value == 1) {
+          document.getElementById("CampoBusqueda").innerHTML = "<input id='nroCaja'class='form-control' placeholder='000'> "
+          $("#nroCaja").focus();
+
+      } else if (sele.options[sele.selectedIndex].value == 2) {
+          document.getElementById("CampoBusqueda").innerHTML = "<label for='fec1'class=''>Desde:</label><input  id='fec1' type='date' class='form-control'><label for='fec2' class=''>Hasta:</label><input  id='fec2' type='date' class='form-control mb-4'>"
+          $("#fec1").focus();
+      } else if (sele.options[sele.selectedIndex].value == 3) {
+          document.getElementById("CampoBusqueda").innerHTML = "<label for='fec3'class=''>Desde:</label><input  id='fec3' type='date' class='form-control'><label for='fec4' class=''>Hasta:</label><input  id='fec4' type='date' class='form-control mb-4'>"
+          $("#fec3").focus();
+      } else if (sele.options[sele.selectedIndex].value == 4) {
+          document.getElementById("CampoBusqueda").innerHTML = "<input  id='razon' class='form-control' placeholder='Nombre...'> "
+          $("#razon").focus();
+      } else if (sele.options[sele.selectedIndex].value == 5) {
+          document.getElementById("CampoBusqueda").innerHTML = `<select name="" id="estado" class="form-control mb-4">
+          <option value="0">Generar por:</option>
+          <option value="1">Activo</option>
+          <option value="2">Ocupado</option>
+        </select>`;
+
+          $("#estado").focus();
+      } else if (sele.options[sele.selectedIndex].value == 6) {
+          document.getElementById("CampoBusqueda").innerHTML = `<select name="" id="condicion" class="form-control mb-4">
+          <option value="0">Generar por:</option>
+          <option value="1">Contado</option>
+          <option value="2">Crédito</option>        
+        </select>`;
+          $("#condicion").focus();
+      }
+  }
+</script>
 
 
 </html>
