b2170acd7e13fe9987431729dc06980cce676045
ROCK3D3L <62083972+ROCK3D3L@users.noreply.github.com>
2021-05-13 23:43:52 -0400

    comenzando el index

--- index.php
+++ index.php
@@ -21,13 +21,74 @@
 </DIv> -->
 
 <div class="container-fluid ">
-  <div class="d-sm-flex justify-content-between align-items-center mb-4">
-    <!-- <h1 class="text-black mb-0">Tablero</h1> -->
-  </div>
-    
-
-  <!-- <img src="img/sicctema.jpeg"/> -->
   
+  <?php
+    require_once("./servicios/conexion.php");
+    $conex = conexion();
+    $sql = "SELECT ci,nomApe,codCand FROM `candidatos` AS r1 
+              JOIN (SELECT CEIL(RAND() * (SELECT MAX(ci) FROM candidatos)) AS id) AS r2
+            WHERE r1.ci >= r2.id
+            ORDER BY r1.ci ASC
+            LIMIT 5";
+    $res = mysqli_query($conex, $sql);
+
+    foreach ($res as $fila) {
+
+
+    }
+  
+  
+  
+  ?>
+
+<div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+      <div class="container-1">
+        <div class="img-content">
+          <div class="img">
+            <div class="info">
+              <h2>
+                nombre candidato
+            </h2>
+            <h4>
+              candidatura
+            </h4>
+          </div>
+          <div class="social">
+            <ul>
+              <li>
+                <a href="#">
+                  <i class="h3 fab fa-facebook-f"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ 
+  
+
+
+
 </div>
 
 <!-- FIN DEL CONTENIDO PRINCIPAL -->
