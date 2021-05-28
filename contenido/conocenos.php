21c2d1fb601ed3984c42482ef7c41d3b2d295a53
emycon99 <emycontreras@unc.edu.py>
2021-05-28 14:01:05 -0400

    cambio de posicion
    nombres segun foto

--- contenido/conocenos.php
+++ contenido/conocenos.php
@@ -33,11 +33,11 @@
               <p class="text-dark p-0 text-justify font-weight">Somos estudiantes del Quinto Año de la carrera de Ingeniería en informática empresarial de la Universidad Nacional de Concepción.</p>
               <p class="text-dark p-0 text-justify font-weight">Hemos desarrollado esta página web “Conócelos” en pos de que la ciudadanía pueda conocer a las personas candidatadas para las Internas del 20 de Junio puesto que por las medidas de seguridad que debemos de tomar con respecto al Covid 19 no se pueden realizar las campañas políticas ni conferencias que eran habituales para que los candidatos den a conocer el motivo por el cual se están postulando a ese puesto y el porqué de elegirlos como gobernantes de nuestra ciudad.</p>
               <p class="text-dark p-0 text-justify font-weight">Esta página web es un proyecto llevado acabo por los siguientes alumnos junto con la tutoría del Profesor Federico Beck, buscamos de la misma manera darnos a conocernos como futuros profesionales en el ámbito correspondiente.</p>
-              <p class="text-dark p-0 text-justify font-weight-bold">1)	Isabel Cuevas </p>
-              <p class="text-dark p-0 text-justify font-weight-bold">2)	Agustina Alfonso</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">1)	Agustina Alfonso</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">2)	Roque Delvalle</p>
               <p class="text-dark p-0 text-justify font-weight-bold">3)	Emy Contreras</p>
-              <p class="text-dark p-0 text-justify font-weight-bold">4)	Roque Delvalle</p>
-              <p class="text-dark p-0 text-justify font-weight-bold">5)	Sandro Castillo</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">4)	Sandro Castillo</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">5)	Isabel Cuevas </p>
               <img style="width: 500px; height: 500px;" class="rounded mx-auto d-block" src="../img/fotoGrupal.jpg" alt="Cargando imagen...">
             </li>
           </ul>
