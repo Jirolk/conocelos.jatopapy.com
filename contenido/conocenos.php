15f74011546a7dd5837cc88f6f8b4f79f4928f76
emycon99 <emycontreras@unc.edu.py>
2021-05-27 21:03:42 -0400

    Termine conocenos
    Le di color a la pagina y agregue la imagen

--- contenido/conocenos.php
+++ contenido/conocenos.php
@@ -15,10 +15,10 @@
 </head>
 <?php require_once "vistas/parte_superior.php"; ?>
 <!-- INICIO DEL "CONOCENOS" -->
-
+<!--
 <DIv class="container">
   <h1>CONÓCENOS</h1>
-</DIv>
+</DIv>-->
 
 <div class="container-fluid ">
   <div id="datPersDet" class="row">
@@ -27,17 +27,18 @@
         <h1 class="text-dark text-center font-weight-bold ">CONÓCENOS</h1>
       </div>
       <div class=" mb-2  ">
-        <div class="  align-items-center">
+        <div class=" card py-2 r2 align-items-center">
           <ul>
             <li>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">Somos estudiantes del Quinto Año de la carrera de Ingeniería en informática empresarial de la Universidad Nacional de Concepción.</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">Hemos desarrollado esta página web “Conócelos” en pos de que la ciudadanía pueda conocer a las personas candidatadas para las Internas del 20 de Junio puesto que por las medidas de seguridad que debemos de tomar con respecto al Covid 19 no se pueden realizar las campañas políticas ni conferencias que eran habituales para que los candidatos den a conocer el motivo por el cual se están postulando a ese puesto y el porqué de elegirlos como gobernantes de nuestra ciudad.</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">Esta página web es un proyecto llevado acabo por los siguientes alumnos junto con la tutoría del Profesor Federico Beck, buscamos de la misma manera darnos a conocernos como futuros profesionales en el ámbito correspondiente.</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">1)	Isabel Cuevas </p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">2)	Agustina Alfonso</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">3)	Emy Contreras</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">4)	Roque Delvalle</p>
-              <p class="text-uppercase text-dark p-0 text-justify font-weight">5)	Sandro Castillo</p>
+              <p class="text-dark p-0 text-justify font-weight">Somos estudiantes del Quinto Año de la carrera de Ingeniería en informática empresarial de la Universidad Nacional de Concepción.</p>
+              <p class="text-dark p-0 text-justify font-weight">Hemos desarrollado esta página web “Conócelos” en pos de que la ciudadanía pueda conocer a las personas candidatadas para las Internas del 20 de Junio puesto que por las medidas de seguridad que debemos de tomar con respecto al Covid 19 no se pueden realizar las campañas políticas ni conferencias que eran habituales para que los candidatos den a conocer el motivo por el cual se están postulando a ese puesto y el porqué de elegirlos como gobernantes de nuestra ciudad.</p>
+              <p class="text-dark p-0 text-justify font-weight">Esta página web es un proyecto llevado acabo por los siguientes alumnos junto con la tutoría del Profesor Federico Beck, buscamos de la misma manera darnos a conocernos como futuros profesionales en el ámbito correspondiente.</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">1)	Isabel Cuevas </p>
+              <p class="text-dark p-0 text-justify font-weight-bold">2)	Agustina Alfonso</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">3)	Emy Contreras</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">4)	Roque Delvalle</p>
+              <p class="text-dark p-0 text-justify font-weight-bold">5)	Sandro Castillo</p>
+              <img style="width: 500px; height: 500px;" class="rounded mx-auto d-block" src="../img/fotoGrupal.jpg" alt="Cargando imagen...">
             </li>
           </ul>
         </div>
